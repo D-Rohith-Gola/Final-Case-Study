150fc454c77089d90ed339ef4d6cb870d2e0f18e
D Rohith Gola <golarohi786@gmail.com>
2026-02-16 21:55:09 +0530

    Initial commit for payment service

--- auth-server/src/main/java/com/npci/auth/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.npci.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.npci.auth.entity.AppUser;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<AppUser, Long> {
-    Optional<AppUser> findByUsername(String username);
-}
